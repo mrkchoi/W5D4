f8b279bee9c5aeff73574b98c415c547c65bcef9
mrkchoi <kennethichoi@gmail.com>
2019-06-13 15:03:31 -0700

    Complete #twoSum

--- JS exercises/phase_1_arrays.js
+++ JS exercises/phase_1_arrays.js
@@ -14,7 +14,27 @@ Array.prototype.uniq = function() {
 
 // let array=[1,1,2,3];
 // console.log(array.uniq());
-
+//array=[-1,0,2,1]
 Array.prototype.twoSum = function() {
-  
+  let output=[];
+  let that = this;
+  this.forEach(function(el1, idx1) {
+    if ( idx1 < (that.length - 1)) {
+      that.forEach(function (el2, idx2) {
+        if (el1 + el2 === 0 && idx1 < idx2 ) {
+          output.push(idx1);
+          output.push(idx2);
+        }
+      }); 
+    }
+  }); 
+  return output;
 };
+
+// let arr = [-1, 0, 2, 1];
+// console.log(arr.twoSum());
+
+
+
+
+console.log()
